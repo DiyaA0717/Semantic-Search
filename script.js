6bd553b977a8a034d9533c00f0e06c7298ad8fea
sabanayasmine08 <sabanayasmine87@gmail.com>
2026-03-09 22:56:16 +0530

    Refactor search function for improved query handling
    Updated the search function to include query parameter in the body and formatted results output.

--- script.js
+++ script.js
@@ -1,19 +1,18 @@
-async function search(){
+async function search() {
 
 const query = document.getElementById("query").value;
 
-const res = await fetch("http://localhost:5000/search",{
-method:"POST",
-headers:{
-"Content-Type":"application/json"
+const res = await fetch("http://localhost:5000/search", {
+method: "POST",
+headers: {
+"Content-Type": "application/json"
 },
-body:JSON.stringify({query})
+body: JSON.stringify({ query: query })
 });
 
 const data = await res.json();
 
 document.getElementById("results").innerHTML =
-"<b>Results:</b><br>"+JSON.stringify(data,null,2);
+"<b>Results:</b><br><pre>" + JSON.stringify(data, null, 2) + "</pre>";
 
 }
-
