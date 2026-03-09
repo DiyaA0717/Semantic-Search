283239702c585a934b8b936aff9596f29d2ae91a
sabanayasmine08 <sabanayasmine87@gmail.com>
2026-03-09 12:18:21 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,22 @@
+async function search(){
+
+const query = document.getElementById("query").value
+
+const res = await fetch("http://localhost:5000/search",{
+
+method:"POST",
+
+headers:{
+"Content-Type":"application/json"
+},
+
+body:JSON.stringify({query})
+
+})
+
+const data = await res.text()
+
+document.getElementById("results").innerHTML =
+"<b>Results:</b><br>"+data
+
+}
