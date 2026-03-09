635ab833284d017560982c75b157905954ef65fc
sabanayasmine08 <sabanayasmine87@gmail.com>
2026-03-09 22:07:44 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,33 +1,18 @@
 import express from "express";
 import cors from "cors";
-import path from "path";
-import { fileURLToPath } from "url";
 
 const app = express();
 const PORT = 3000;
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
 app.use(cors());
 app.use(express.json());
+app.use(express.static("./"));
 
-/* Serve frontend files */
-app.use(express.static(__dirname));
-
-/* Open index.html */
-app.get("/", (req, res) => {
-  res.sendFile(path.join(__dirname, "index.html"));
-});
-
-/* Search API */
 app.post("/search", (req, res) => {
   const query = req.body.query;
-
   res.send("You searched: " + query);
 });
 
-/* Start server */
 app.listen(PORT, () => {
-  console.log(`Server running at http://localhost:${PORT}`);
+  console.log("Server running at http://localhost:3000");
 });
